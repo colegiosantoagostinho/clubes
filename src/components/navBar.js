a8a81dc687722aa0b88be9b47fcd48b28232fe23
Alexandre Carneiro <72991728+SDAlez@users.noreply.github.com>
2022-07-29 21:02:48 +0200

    Update for mobile devices

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -7,9 +7,9 @@ const NavBar = () => {
         ...theme.typography.button,
     }));
 
-    return <AppBar position="static" color="primary" sx={{minWidth:"100%"}}>
+    return <AppBar position="static" color="primary" sx={{minWidth:"100vw", maxWidth:"100vw"}}>
         <Toolbar style={{margin:"auto"}}>
-            <StyledTypography sx={{fontSize:36, minWidth:"100%"}}>
+            <StyledTypography sx={{fontSize:36}}>
                 Clubes
             </StyledTypography>
         </Toolbar>
