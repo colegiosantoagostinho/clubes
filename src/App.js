b224a1023e2966809b8c910d7197fa2057e2e23b
Alexandre Carneiro <72991728+SDAlez@users.noreply.github.com>
2022-07-29 13:13:43 +0200

    Remove react-routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import './App.css';
 import MainPage from './components/mainPage';
-import { HashRouter, Routes, Route } from 'react-router-dom';
-import LinksPage from './components/linkspage';
 
 function App() {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<MainPage />}/>
-        <Route path="/club/:link" element={<LinksPage />}/>
-      </Routes>
-    </HashRouter>
+    <MainPage/>
   );
 }
 
